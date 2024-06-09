ecfb56034d4daea27d3e8aec4808bd09a732aaf1
Nuggitt <phulip.nielsen@gmail.com>
2024-06-09 15:01:12 +0200

    Add project files.

---
+++ DogLib/DogsRepository.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DogLib
+{
+    public class DogsRepository
+    {
+        private int _nextId = 4;
+
+        private List<Dog> _dogs = new List<Dog>()
+        {
+            new Dog { Id = 1, Name = "Fido", Age = 5 },
+            new Dog { Id = 2, Name = "Fjumse", Age = 2 },
+            new Dog { Id = 3, Name = "Fisen", Age = 4 }
+
+        };
+
+        public IEnumerable<Dog> GetAll()
+        {
+            return new List<Dog>(_dogs);
+        }
+
+        public Dog? GetById(int id)
+        {
+            return _dogs.FirstOrDefault(d => d.Id == id);
+        }
+
+        public Dog Add(Dog dog)
+        {
+            dog.Id = _nextId++;
+            _dogs.Add(dog);
+            return dog;
+        }
+
+        public Dog? Delete(int id)
+        {
+            Dog? dog = GetById(id);
+            if (dog != null)
+            {
+                _dogs.Remove(dog);
+            }
+            return dog;
+        }
+
+
+    }
+}
---
+++ DogLibTests/DogRepositoryTests.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DogLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace DogLib.Tests
+{
+    [TestClass()]
+    public class DogRepositoryTests
+    {
+        private DogsRepository _dogRepository = new DogsRepository();
+
+        [TestMethod()]
+        public void GetAllTest()
+        {
+            List<Dog> dogs = _dogRepository.GetAll().ToList();
+            Assert.AreEqual(3, dogs.Count);
+            Assert.AreEqual("Fido", dogs[0].Name);
+
+            var dogsWithF = dogs.Where(d => d.Name.StartsWith("F"));
+            Assert.AreEqual(3, dogsWithF.Count());
+
+            var dogsWithAgeOver3 = dogs.Where(d => d.Age > 3);
+            Assert.AreEqual(2, dogsWithAgeOver3.Count());
+
+        }
+
+        [TestMethod()]
+        public void GetByIdTest()
+        {
+            Dog? dog = _dogRepository.GetById(2);
+            Assert.IsNotNull(dog);
+            Assert.AreEqual("Fjumse", dog.Name);
+
+        }
+
+        [TestMethod()]
+        public void AddTest()
+        {
+            _dogRepository.Add(new Dog { Name = "Futte", Age = 7 });
+            List<Dog> dogs = _dogRepository.GetAll().ToList();
+            Assert.AreEqual(4, dogs.Count);
+            Assert.AreEqual("Futte", dogs[3].Name);
+        }
+
+        [TestMethod()]
+        public void DeleteTest()
+        {
+            _dogRepository.Delete(2);
+            List<Dog> dogs = _dogRepository.GetAll().ToList();
+            Assert.AreEqual(2, dogs.Count);
+            Assert.IsNull(_dogRepository.GetById(2));
+        }
+    }
+}
